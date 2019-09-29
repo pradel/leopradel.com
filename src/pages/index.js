20673bcbcdf80aee35c9dea91c4ff07adb8b2c2a
pradel <pradel.leo@gmail.com>
2019-09-29 12:34:58 +0200

    Add projects list

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,55 @@ import SEO from '../components/seo';
 import twitter from '../icons/twitter.svg';
 import github from '../icons/github.svg';
 
+const projects = [
+  {
+    name: 'accounts-js',
+    description:
+      'Fullstack authentication and accounts-management for JavaScript.',
+    githubUrl: 'https://github.com/accounts-js/accounts',
+  },
+  {
+    name: 'Sigle',
+    description: 'A beautiful decentralized and open source blog maker.',
+    githubUrl: 'https://github.com/pradel/sigle',
+  },
+  {
+    name: 'react-responsive-modal',
+    description: 'A simple responsive and accessible react modal.',
+    githubUrl: 'https://github.com/pradel/react-responsive-modal',
+  },
+  {
+    name: 'Twoblocks',
+    description: 'Free and open source 2fa manager built with Blockstack.',
+    githubUrl: 'https://github.com/pradel/twoblocks',
+  },
+  {
+    name: 'node-intagram',
+    description: 'Instagram api client for node that support promises.',
+    githubUrl: 'https://github.com/pradel/node-instagram',
+  },
+  {
+    name: 'react-google-photo',
+    description: 'React lightbox component using the google photo style.',
+    githubUrl: 'https://github.com/pradel/react-google-photo',
+  },
+  {
+    name: 'react-minimalist-portal',
+    description: 'A minimalist portal for react.',
+    githubUrl: 'https://github.com/pradel/react-minimalist-portal',
+  },
+  {
+    name: 'react-blockstack-button',
+    description: 'React component to display a login with blockstack button.',
+    githubUrl: 'https://github.com/pradel/react-blockstack-button',
+  },
+  {
+    name: 'craco-blockstack',
+    description: 'A craco plugin to use Blockstack with create-react-app.',
+    githubUrl: 'https://github.com/pradel/craco-blockstack',
+  },
+];
+
 const Home = () => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -22,20 +71,37 @@ const Home = () => {
   `);
 
   return (
-    <div className="container">
+    <React.Fragment>
       <SEO title="Home" />
 
-      <h1>{data.site.siteMetadata.title}</h1>
-      <p>{data.site.siteMetadata.description}</p>
-      <div className="social-icons">
-        <a href={data.site.siteMetadata.twitter} target="_blank">
-          <img src={twitter} alt="twitter" />
-        </a>
-        <a href={data.site.siteMetadata.github} target="_blank">
-          <img src={github} alt="github" />
-        </a>
+      <div className="container-head">
+        <h1>{data.site.siteMetadata.title}</h1>
+        <p>{data.site.siteMetadata.description}</p>
+        <div className="social-icons">
+          <a href={data.site.siteMetadata.twitter} target="_blank">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href={data.site.siteMetadata.github} target="_blank">
+            <img src={github} alt="github" />
+          </a>
+        </div>
       </div>
-    </div>
+
+      <div className="container-projects">
+        <h1>Some projects</h1>
+
+        <div className="list">
+          {projects.map((project, index) => (
+            <article key={index}>
+              <a href={project.githubUrl}>
+                <span className="title">{project.name}</span>
+                <span className="description">{project.description}</span>
+              </a>
+            </article>
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
