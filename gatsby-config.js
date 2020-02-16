9762e19d56a2b334177ecc9a845582f5e24ed35a
Pradel Léo <pradel.leo@gmail.com>
2020-02-16 13:32:56 +0100

    Setup blog structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,39 @@ module.exports = {
     github: 'https://github.com/pradel',
     author: `@leopradel`,
   },
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+  ],
 };
