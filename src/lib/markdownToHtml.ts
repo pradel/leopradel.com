ff3809c074320a8c50053a324300d84ab989e73a
pradel <pradel.leo@gmail.com>
2020-03-30 22:31:03 +0200

    Update markdownToHtml.ts

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -6,7 +6,6 @@ import autolinkHeadings from 'remark-autolink-headings';
 export const markdownToHtml = async (markdown: string) => {
   const result = await remark()
     .use(slug)
-    // TODO setup an clickable icon https://github.com/remarkjs/remark-autolink-headings
     .use(autolinkHeadings)
     .use(html)
     .process(markdown);
