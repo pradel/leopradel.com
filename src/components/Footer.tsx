be9b1383d76674d7754f170fa2d0da8b8045cbee
pradel <pradel.leo@gmail.com>
2020-04-02 15:18:31 +0200

    producthunt footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ export const Footer = () => {
             }}
           />
         </li>
-        <li>
+        <li className="mr-3">
           <a
             className="inline-block border border-white rounded hover:border-gray-200 text-gray-600 hover:bg-gray-200 p-3"
             href={config.twitterUrl}
@@ -24,6 +24,17 @@ export const Footer = () => {
             rel="noopener noreferrer"
             dangerouslySetInnerHTML={{
               __html: require('../icons/twitter.svg?include'),
+            }}
+          />
+        </li>
+        <li>
+          <a
+            className="inline-block border border-white rounded hover:border-gray-200 text-gray-600 hover:bg-gray-200 p-3"
+            href={config.productHunturl}
+            target="_blank"
+            rel="noopener noreferrer"
+            dangerouslySetInnerHTML={{
+              __html: require('../icons/producthunt.svg?include'),
             }}
           />
         </li>
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   twitterUrl: 'https://twitter.com/leopradel',
   githubUrl: 'https://github.com/pradel',
+  productHunturl: 'https://www.producthunt.com/@leopradel',
   fathomSiteId: 'VMXKJESU',
 };
