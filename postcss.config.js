933c675fbc7a0331fb2c20f7225c9ddd7a256227
pradel <pradel.leo@gmail.com>
2020-04-30 16:20:00 +0200

    feat: upgrade deps

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,6 @@ module.exports = {
   plugins: [
     'tailwindcss',
     'postcss-nesting',
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: ['./src/**/*.{js,jsx,ts,tsx,svg}'],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
     'postcss-preset-env',
   ],
 };
