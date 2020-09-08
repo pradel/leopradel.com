52c2bb0dd2ca32fae7de480a19511a63bd7bef74
pradel <pradel.leo@gmail.com>
2020-09-08 11:22:11 +0200

    chore: upgrade deps

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-nesting',
-    'postcss-preset-env',
-  ],
+  plugins: ['tailwindcss', 'postcss-nesting', 'postcss-preset-env'],
 };
