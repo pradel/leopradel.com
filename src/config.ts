53fcb2d7422296325aa56d4f6ed8570caaa0a317
pradel <pradel.leo@gmail.com>
2020-09-10 14:21:02 +0200

    Add rss icon

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ export const config = {
   twitterUrl: 'https://twitter.com/leopradel',
   githubUrl: 'https://github.com/pradel',
   productHunturl: 'https://www.producthunt.com/@leopradel',
+  rssUrl: 'https://www.leopradel.com/feed',
   fathomSiteId: 'VMXKJESU',
 };
