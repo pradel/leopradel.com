9c339868f64c7558b6731a64fa41cdc0d640adf4
Léo Pradel <pradel.leo@gmail.com>
2020-11-21 16:57:59 +0100

    chore: upgrade tailwind to 2 (#26)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  future: {
-    // Remove once v2 is released
-    purgeLayersByDefault: true,
-    // Remove once v2 is released
-    removeDeprecatedGapUtilities: true,
-  },
-  purge: ['./src/**/*.tsx', './src/icons/**/*.svg'],
+  purge: ['./src/**/*.{ts,tsx}', './src/icons/**/*.svg'],
   theme: {
     extend: {
       colors: {
+        gray: colors.blueGray,
         watermelon: '#FD4659',
       },
-    },
-    typography: {
-      default: {
-        css: {
-          'code::before': {
-            content: '',
-          },
-          'code::after': {
-            content: '',
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '',
+            },
+            'code::after': {
+              content: '',
+            },
           },
         },
       },
     },
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [require('@tailwindcss/typography')],
 };
