90b182c5da4b396edb6f794e980faf920ea06e29
Léo Pradel <pradel.leo@gmail.com>
2023-04-30 13:23:15 +0200

    new api sitemap

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   rewrites() {
     return [
       {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-      {
         source: '/feed',
         destination: '/api/rss',
       },
