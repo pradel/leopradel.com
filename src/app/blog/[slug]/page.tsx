46f666765fabad0882746d923b0d37f3a27ed2a8
Léo Pradel <pradel.leo@gmail.com>
2023-04-30 16:53:58 +0200

    use balancer

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import readingTime from 'reading-time';
+import Balancer from 'react-wrap-balancer';
 import { format } from 'date-fns';
 import { join } from 'path';
 import { readFileSync } from 'fs';
@@ -58,15 +59,15 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <article>
-      <header>
-        <h1 className="font-sans text-4xl font-bold leading-tight text-black">
-          {post.title}
+      <header className="text-center">
+        <h1 className="font-sans text-4xl font-semibold leading-tight text-black">
+          <Balancer>{post.title}</Balancer>
         </h1>
-        <p className="mb-4 mt-1 text-sm text-gray-500">
+        <p className="mt-2 text-sm text-gray-600">
           {post.date} • {post.readingTime}
         </p>
       </header>
-      <hr className="my-8 border-b-2 border-gray-100" />
+      <hr className="my-8 md:my-12 border-b-2 border-gray-100" />
       <section
         className="prose lg:prose-lg prose-h1:font-sans prose-h2:font-sans prose-h3:font-sans prose-h4:font-sans"
         dangerouslySetInnerHTML={{ __html: post.content }}
