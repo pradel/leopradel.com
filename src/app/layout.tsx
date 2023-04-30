1ac351375088310b71441be79c3cf4de24b04dc9
Léo Pradel <pradel.leo@gmail.com>
2023-04-30 13:14:10 +0200

    setup directory

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={clsx(inter.className)}>{children}</body>
+    </html>
+  );
+}
